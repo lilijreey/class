c2e2d4aa676fb773324effd2746b2458865fc697
zl <zl@santi.com>
2016-01-08 18:35:36 +0800

    ic

--- except/e2.cpp
+++ except/e2.cpp
@@ -4,17 +4,79 @@
 #include <exception>
 
 struct A {
-  int i;
-  A(int i):i(i){
-    printf("A::start i:%d\n",i);
-    if (i > 3) 
-      throw i;
-    printf("A::end\n");
+  A(int l) {}
+  int len;
+  //char *buf;
+  //A(int len){
+    //char *p= malloc(len);
+    //if (NULL == buf) {
+      //throw std::bad_alloc;
+    //}
+
+    //buf = p;
+    //char *p= malloc(len);
+    //if (NULL == buf) {
+      //free(p)
+      //throw std::bad_alloc;
+    //}
+    //buf = p;
+
+    //printf("A::start i:%d\n",i);
+    //if (i > 3) 
+      //throw i;
+    //printf("A::end\n");
+  };
+  //~A() {
+    //free(buf);
+    //printf("A::~() i:%d\n", i);
+  //}
+//void ok() noexcept {
+  //printf("ok\n");
+//}
+
+struct D {
+  virtual void ok() {
+    printf("ok\n"); 
   }
-  ~A() {
-    printf("A::~() i:%d\n", i);
+  virtual void ok() noexcept{
+    printf("ok\n");
+  }
+
+  void cc(int *) {}
+  void cc(const int *) {}
+
+};
+
+struct F : D {
+  virtual void ok() noexcept {
+    printf("ok\n");
   }
 };
+
+void ok(int i) throw(int, double)
+{
+  switch (i) {
+  case 1:
+    throw 3;
+  case 2:
+    throw 3.23;
+  case 3:
+    throw A(3);
+  }
+
+  throw 3;
+
+}
+
+int main(int argc, char *argv[])
+{
+  
+  return 0;
+}
+
+
+#if 0
+
 
 void oo() {
   A a(2);
@@ -22,9 +84,38 @@ void oo() {
   printf("oo\n");
 }
 
+struct MutexScope {
+  //Mutex() {
+    //m.lock();
+  //}
+  //pthread_mutex m;
+  //~Mutex() {
+    //m.unlock();
+  //}
+};
+
 int main(int argc, char *argv[])
 {
-  oo();
+  //list l;
+  //pthread_mutex m;
+  //m.lock();
+  //l.insert(3);
+  //m.unlock();
+  //
+
+//RAII
+ //{ MutexScope mc;
+  //l.insert(3);
+ //}
+
+
+  try {
+    oo();
+  }
+  catch(...) {
+  }
+  printf("ok\n");
+
   
   return 0;
 }
@@ -62,4 +153,7 @@ void yy() try {
   ////std::terminate();
 
   //printf("world\n");
+  //
+  
 //}
+#endif 
