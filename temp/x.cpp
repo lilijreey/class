0e0cf9ff813ff25dfbce911fe540be6a421e50bf
zl <zl@santi.com>
2016-01-17 10:10:08 +0800

    ic

--- temp/x.cpp
+++ temp/x.cpp
@@ -1,130 +1,115 @@
 #include <limits>
 #include <iostream>
-#include <stdio.h>
-#include <typeinfo>
-#include <assert.h>
-
-struct Z {
-  Z(int s=0, double o =1) :s(s), o(o){}
-  int s;
-  double o;
-  void show() {
-    printf("%d:%g\n",s, o);
-  }
-};
-
-//void swap(int &a, int &b) {
-  //printf("swap int\n");
-  //int tmp = a;
-  //a = b;
-  //b = tmp;
-//}
-//void swap(double &a, double &b) {
-  //printf("swap double\n");
-  //double tmp = a;
-  //a = b;
-  //b = tmp;
-//}
 
 //template <typename T>
-//void swap(T &a, T &b) ;
-//{
-  //printf("swap template\n");
-  //xxxoo(32);
-  //oooxx(false);
-  //T tmp = a;
-  //a = b;
-  //b = tmp;
+//T zero() { return T{}; }
+//int main() {
+  //int i = zero<int>();
+  //double d = zero<double>();
+  //zero();
 //}
-#if 0
-
-int main(int argc, char *argv[])
-{
-  //int a=1,b=22;
-  //swap(a,b); //<int,int>
-  //assert(a==22);
-  //assert(b==1);
-
-  Z a1(11,1.11);
-  Z a2(22,2.22);
-  swap(a1, a2);
-  a1.show();
-  a2.show();
-  
-  return 0;
-}
-
-
-template<typename T> void fnG(T a)
- {
-   printf("fnG name:%s\n", typeid(T).name());
- }
-template<typename T> void fnR(const T &a)
-{
-   printf("fnG name:%s\n", typeid(T).name());
-}
-template<typename T> void fnP(const T *a)
-{
-   printf("fnG name:%s\n", typeid(T).name());
-} 
-
-int main() {
-  int a=3;
-  fnG<int>(a);
-
-  //a=4;
-  //fnR(a);
-  //fnP(a);
-
-  //fnG(&a);
-  //int *p = &a;
-  //fnR(p);
-  //fnP(&a);
-
-
-}
 
 template <typename T>
-T max_limit(T a) {
-  return std::numeric_limits<T>::max();
+void swap(T &l, T &r) {
+  T tmp = l;
+  l = r;
+  r = tmp;
 }
 
-template <typename T, typename E >
-void xx(T t, E e) {
-  std::cout << t << " " << e << std::endl;
+void swap(int &l, int &r) {
+  int tmp = l;
+  l = r;
+  r = tmp;
 }
 
-template <typename T, typename E >
-void xx(T a) {
-  E e;
-  e.show();
-  std::cout << t << " " << e << std::endl;
+void swap(double &l, double &r) {
+  double tmp = l;
+  l = r;
+  r = tmp;
 }
 
+//template <typename T, typename E>
+//void swap(T &l, E &r) {
+  //T tmp = l;
+  //l = r;
+  //r = tmp;
+//}
+
+struct A{};
+struct B{};
 
 
-
-int max_limit(int) { return std::numeric_limits<int>::max();}
-//bool max_limit(bool) { std::numeric_limits<bool>::max();}
-//float max_limit(float) { std::numeric_limits<int>::max();}
-//double max_limit(double) { std::numeric_limits<int>::max();}
-//unsigned long max_limit(unsigned long) { std::numeric_limits<int>::max();}
-//int max_limit(int i) { return std::numeric_limits<int>::max(); }
-
-struct A {
-};
 int main(int argc, char *argv[])
 {
-  xx(1,3.23);
-  //std::cout << max_limit(a) << std::endl;
-  //std::cout << max_limit<double>(3.33)<< std::endl;
-  //std::cout << max_limit(false)<< std::endl;
-  //std::cout << std::numeric_limits<int>::max() << std::endl;
-  //std::cout << std::numeric_limits<double>::max() << std::endl;
-  //std::cout << std::numeric_limits<unsigned long>::max() << std::endl;
-  //std::cout << std::numeric_limits<float>::max() << std::endl;
-  //std::cout << std::numeric_limits<bool>::max() << std::endl;
+  int i=11, j=99;
+  swap<int>(i,j);
+  printf("%d %d\n",i,j);
+  
+  double m=11, n=99;
+  swap(n,m);
+  return 0;
+}
+//void swap(int l, int r) { }
 
+#if 0
+
+template <typename T>
+T max_limit(T obj) { 
+  printf("max_limit template \n");
+  return std::numeric_limits<T>::max();
+}
+double max_limit(double) { 
+  printf("max_limit double\n");
+  return std::numeric_limits<double>::max();
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//template <typename T>
+//void show(T &v1, T &v2)
+//{
+  //T tmp = v1;
+  //v1 = v2;
+  //v2 = tmp;
+//}
+
+
+//int max_limit(int) {
+  //return std::numeric_limits<int>::max();}
+//unsigned long max_limit(unsigned long) { 
+  //return std::numeric_limits<int>::max();}
+int main(int argc, char *argv[])
+{
+
+  std::cout << max_limit<double>(1.323) << std::endl;
+  std::cout << max_limit(1.323) << std::endl;
+  std::cout << max_limit(32) << std::endl;
+  //std::cout << max_limit<double, int>(3.0f) << std::endl;
+  //std::cout << max_limit(false) << std::endl;
+  //std::cout << max_limit(32LU) << std::endl;
+  //std::cout << max_limit(A) << std::endl;
+
+}
+  //std::cout << 
+      //std::numeric_limits<int>::max() << std::endl;
+  //std::cout 
+      //<< std::numeric_limits<double>::max() << std::endl;
+  //std::cout 
+      //<< std::numeric_limits<unsigned long>::max() << std::endl;
+  //std::cout 
+      //<< std::numeric_limits<float>::max() << std::endl;
+  //std::cout
+      //<< std::numeric_limits<bool>::max() << std::endl;
+//}
 #endif
--- temp/y.cpp
+++ temp/y.cpp
@@ -1,19 +1,51 @@
-/**
- * @file     y.cpp
- *           
- *
- * @author   lili  <lilijreey@gmail.com>
- * @date     12/27/2015 05:12:11 PM
- *
- */
-#include <stdlib.h>
+#include <iostream>
+#include <vector>
+template <typename T>
+class X{
+ public:
+  T  value;
+  void show() const;
+  X<T>& operator++();
+  void xx() {
+    double f = NULL + 1;
+  }
+};
+//template <typename T>
+//void X<T>::show() const {
+//  std:: cout << value << std::endl;
+//}
+//template <typename T>
+//X<T>& X<T>::operator++() {
+//  value++;
+//  return *this;
+//}
 
-#include "x.hpp"
+struct O {
+  int _v;
+  O(int i=0) : _v(i) {}
+};
 
 int main(int argc, char *argv[])
 {
+  //X<int> a;
+  //X<double> b;
+  //a.value=1;
+  //++a;
+  //a.show();
+  //b.show();
+
+
+  //a.value =12;
+  //a.show();
+
+  //std::vector<int> v = {1,2,3,4,5,6};
+  //for (auto &e : v) printf("%d", e);
+
+  //printf("\n");
+  //std::vector<O> o = {1,2,3,4,5,6};
+  //for (auto &e : o) printf("%d", e._v);
+
+
   
-  int a=3, b=5;
-  swap(a, b);
   return 0;
 }
